a7c064a61e9ec4a0b71b4c5b832a6ed650c19825
DevOsi123 <gatelogistics@consultant.com>
2025-09-11 13:02:03 -0700

    new

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,18 @@
+// const x = parseInt(process.argv[2]);
+
+// if (isNaN(x)){
+//     console.log("Missing number of occurences");
+// }
+// else{
+//     for(let i = 0; i < x; i++) console.log("C is fun");
+// }
+
+
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x)){
-    console.log("Missing number of occurences");
-}
-else{
-    for(let i = 0; i < x; i++) console.log("C is fun");
+if (isNaN(x) || x <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  const lines = "C is fun\n".repeat(x).trim().split('\n');
+  for (const line of lines) console.log(line);
 }
