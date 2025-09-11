cd2e64f1db4ffe7a4595ca1ff6f0340e2cd2a87d
DevOsi123 <gatelogistics@consultant.com>
2025-09-11 12:45:40 -0700

    new

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,9 +1,18 @@
+// const lines = [
+//     "C is FUN",
+//     "Python is cool",
+//     "Javascript is amazing"
+// ];
+
+// for(let i = 0; i < lines.length; i++){
+//     console.log(lines[i])
+// }
+
+
 const lines = [
-    "C is FUN",
-    "Python is cool",
-    "Javascript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
-for(let i = 0; i < lines.length; i++){
-    console.log(lines[i])
-}
+for (const line of lines) console.log(line);
