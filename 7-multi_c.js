bbe6f2779b73d5a405c7f6b637290bfaf7f79c0a
DevOsi123 <gatelogistics@consultant.com>
2025-09-11 13:03:59 -0700

    new

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -8,11 +8,21 @@
 // }
 
 
+// const x = parseInt(process.argv[2]);
+
+// if (isNaN(x) || x <= 0) {
+//   console.log("Missing number of occurrences");
+// } else {
+//   const lines = "C is fun\n".repeat(x).trim().split('\n');
+//   for (const line of lines) console.log(line);
+// }
+
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
-  const lines = "C is fun\n".repeat(x).trim().split('\n');
+  // If x <= 0, repeat creates empty string, so no output
+  const lines = "C is fun\n".repeat(x).trim().split('\n').filter(line => line.length > 0);
   for (const line of lines) console.log(line);
 }
