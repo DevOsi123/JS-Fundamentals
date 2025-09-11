1904160964e52eb875346a5a178548edbf85066c
DevOsi123 <gatelogistics@consultant.com>
2025-09-11 11:09:36 -0700

    added new file

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const number = 10;
+if(number){
+    console.log("No Argument")
+}
+else if(number === 10){
+    console.log("Argument Found")
+}
+else if(number != 10 || number < 10){
+    console.log("Arguments Found")
+}
